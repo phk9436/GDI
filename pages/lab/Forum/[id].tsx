3b8b5710fbd2b71afb514498b9f2ad10d3a94417
digitalworks_hgpark <phk9436@naver.com>
2022-12-19 11:20:14 +0900

    [Feat&Ui] 포럼 상세페이지 구현완료(메인)

--- pages/lab/Forum/[id].tsx
+++ pages/lab/Forum/[id].tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import { BreadCrumb } from 'components/Components';
+import { useRouter } from 'next/router';
+import { doc, getDoc } from 'firebase/firestore';
+import { dbService } from 'api/firebase';
+import { IForumData } from 'types/dataTypes';
+import { useEffect } from 'react';
+import BoardDetail from 'components/lab/BoardDetail';
+import dayjs from 'dayjs';
 
-function ForumDetail() {
-  return <div>ForumDetail</div>;
+function ForumDetail(props: IForumData) {
+  const router = useRouter();
+  const Tap = [
+    [
+      '연구보고서',
+      '경기도 북부지역의 정책방향 및 대안 수립을 위한 기본 · 정책 · 수탁 · 기타 · 공공투자에 대한 연구자료 입니다.',
+      '/lab',
+    ],
+    ['학술포럼', '안내내용', '/lab/Forum'],
+  ];
+  useEffect(() => {
+    if (!props.title) {
+      alert('잘못된 접근입니다');
+      router.push('/lab/Forum');
+    }
+  }, []);
+  return <div>
+  <BreadCrumb category={Tap[1]} tap={Tap} />
+  <BoardDetail
+    data={{
+      ...props,
+      id: router.query.id as string,
+      date: dayjs(props.createdAt).format('YY-MM-DD'),
+    }}
+    path={Tap[1][2]}
+    category="forum"
+  />
+</div>;
 }
 
 export default ForumDetail;
+
+export const getServerSideProps = async ({ params }: { params: IForumData }) => {
+  const data = await getDoc(doc(dbService, 'forum', `${params?.id}`));
+  const contentData = await getDoc(doc(dbService, 'forumContent', `${params?.id}`));
+  return { props: { ...data.data(), ...contentData.data() } };
+};
