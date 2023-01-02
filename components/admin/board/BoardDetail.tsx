7771297197701b42ed07ed2948f04e4df76beaf8
digitalworks_hgpark <phk9436@naver.com>
2023-01-02 17:41:05 +0900

    [Feat&Ui] 어드민 연구제안 상세 기능, ui 작업

---
+++ components/admin/board/BoardDetail.tsx
@@ -0,0 +1,194 @@
+import styled from 'styled-components';
+import { IBoardData } from 'types/dataTypes';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+
+interface IBoardDetailProps {
+  data: IBoardData;
+  deleteBoardItem: (id: string) => Promise<void>;
+}
+
+const PostViewer = dynamic(() => import('components/viewer/Viewer'), {
+  ssr: false,
+});
+
+function BoardDetail({ data, deleteBoardItem }: IBoardDetailProps) {
+
+  return (
+    <Wrapper>
+      <DetailTop>
+        <DetailTopContainer>
+          <p>{data.date}</p>
+          <h3>{data.title}</h3>
+          <DetailFlex>
+            <InfoWrapper>
+              <ul>
+                <li>
+                  <p>작성자</p>
+                </li>
+                <li>{data.author}</li>
+              </ul>
+              <ul>
+                <li>
+                  <p>이메일</p>
+                </li>
+                <li>{data.email}</li>
+              </ul>
+            </InfoWrapper>
+            <Buttons>
+              <Button onClick={() => deleteBoardItem(data.id as string)}>삭제</Button>
+            </Buttons>
+          </DetailFlex>
+        </DetailTopContainer>
+      </DetailTop>
+      <DetailBody>
+        <ContentWrapper>
+          <PostViewer content={data.content as string} />
+        </ContentWrapper>
+        <Link href="/admin/board">
+          <a>
+            <ButtonBack>목록으로 이동</ButtonBack>
+          </a>
+        </Link>
+      </DetailBody>
+    </Wrapper>
+  );
+}
+
+export default BoardDetail;
+
+const Wrapper = styled.div`
+  max-width: 1440px;
+  padding: 56px 60px 140px;
+  margin: auto;
+`;
+
+const DetailTop = styled.div`
+  margin: 0 0 36px;
+  display: flex;
+  align-items: flex-end;
+  gap: 60px;
+  border-bottom: 1px solid #000000;
+  padding-bottom: 47px;
+`;
+
+const DetailTopContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  & > p {
+    width: 112px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(0, 0, 0, 0.5);
+    border-radius: 15px;
+    color: #fff;
+    font-size: 18px;
+  }
+
+  h3 {
+    font-size: 36px;
+    font-weight: 500;
+    line-height: 50px;
+    height: 150px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+  }
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  ul {
+    display: flex;
+    align-items: center;
+
+    li {
+      font-size: 18px;
+
+      p {
+        width: 74px;
+      }
+
+      &:first-child {
+        font-weight: 700;
+        display: flex;
+        align-items: center;
+
+        &::after {
+          content: '';
+          display: block;
+          width: 1px;
+          height: 28px;
+          background-color: #000;
+          margin: 0 18px;
+        }
+      }
+    }
+  }
+`;
+
+const DetailBody = styled.div``;
+
+const ContentWrapper = styled.div`
+  padding: 28px;
+  background-color: #f7f7f7;
+  min-height: 400px;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 30px;
+  color: #5b5859;
+`;
+
+const ButtonBack = styled.div`
+  width: 186px;
+  height: 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 22px;
+  padding-right: 26px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+  background: #1f4788 url('/images/arrowLinkBack.png') no-repeat left 20px center/28px 28px;
+  cursor: pointer;
+`;
+
+const DetailFlex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 4px;
+  min-width: 186px;
+`;
+
+const Button = styled.div`
+  width: 100%;
+  height: 50px;
+  background-color: #c7c7c7;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  color: #fff;
+  transition: 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #a4a4a4;
+  }
+`;
---
+++ pages/admin/board/[id].tsx
@@ -0,0 +1,60 @@
+import { BreadCrumb } from 'components/Components';
+import { useRouter } from 'next/router';
+import { doc, getDoc } from 'firebase/firestore';
+import { dbService } from 'api/firebase';
+import { IBoardData } from 'types/dataTypes';
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import BoardDetail from 'components/admin/board/BoardDetail';
+import Loading from 'components/admin/Loading';
+import { deleteBoardData } from 'utils/deleteBoardUtils';
+
+function Detail(props: IBoardData) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const Tap = [
+    [
+      '연구제안',
+      '일반 사용자들이 GDI에게 자유롭게 제안하는 연구 주제입니다. 관리자와 글 작성자만 확인가능합니다.',
+      '/admin/board',
+    ],
+  ];
+
+  const deleteBoardItem = async (id: string) => {
+    setIsLoading(true);
+    await deleteBoardData(id);
+    alert('삭제되었습니다');
+    router.push('/admin/board');
+  };
+
+  useEffect(() => {
+    if (!props.title) {
+      alert('잘못된 접근입니다');
+      router.push('/admin/board');
+    }
+  }, []);
+
+  return <>
+  <div>
+    <BreadCrumb category={Tap[0]} tap={Tap} />
+    <BoardDetail
+      data={{
+        ...props,
+        id: router.query.id as string,
+        date: dayjs(props.createdAt).format('YY-MM-DD'),
+      }}
+      deleteBoardItem={deleteBoardItem}
+    />
+  </div>
+  {isLoading && <Loading />}
+</>;
+}
+
+export default Detail;
+
+export const getServerSideProps = async ({ params }: { params: IBoardData }) => {
+  const data = await getDoc(doc(dbService, 'board', `${params?.id}`));
+  const contentData = await getDoc(doc(dbService, 'boardContent', `${params?.id}`));
+  return { props: { ...data.data(), ...contentData.data() } };
+};
