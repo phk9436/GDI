480252bcc85ab9ad1a103825b9d4b6341dcd9238
digitalworks_hgpark <phk9436@naver.com>
2023-01-10 13:08:43 +0900

    [Ui] 메인 학술포럼 섹션 ui수정

--- components/main/MainSectionForum.tsx
+++ components/main/MainSectionForum.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { IForumData } from 'types/dataTypes';
 import { SectionTop, Cards } from './Layout';
+import { RedirectDetail } from './Components';
 
 interface PageProps {
   data: IForumData[];
@@ -9,12 +10,19 @@ interface PageProps {
 function MainSectionForum({ data }: PageProps) {
   return (
     <Wrapper>
-      <SectionTop title={'학술포럼'} href="/lab/Forum" />
+      <SectionTop title={'학술포럼 & 이벤트'} href="/lab/Forum" />
       <SectionContents>
         <Cards data={data} path="/lab/Forum" />
-        <DescriptText>
-          모두가 참여가능한 <em>GDI의 학술포럼</em>에 여러분을 초대합니다!
-        </DescriptText>
+        <DescriptWrapper>
+          <DescriptText>
+            모두가 참여가능한 <em>GDI의 학술포럼</em>에 <br />
+            여러분을 초대합니다!
+          </DescriptText>
+          <DescriptLink>
+            학술포럼 리스트
+            <RedirectDetail href="/lab" />
+          </DescriptLink>
+        </DescriptWrapper>
       </SectionContents>
     </Wrapper>
   );
@@ -23,7 +31,6 @@ function MainSectionForum({ data }: PageProps) {
 export default MainSectionForum;
 
 const Wrapper = styled.section`
-  padding: 0 60px;
   max-width: 1300px;
   margin: auto;
   margin-top: 24px;
@@ -37,11 +44,24 @@ const Wrapper = styled.section`
 const SectionContents = styled.div`
   display: flex;
   justify-content: space-between;
+  padding: 0 60px;
+
+  @media screen and (max-width: 820px) {
+    padding: 0;
+  }
+`;
+
+const DescriptWrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+  gap: 34px;
+  width: calc(100% - 722px);
 `;
 
 const DescriptText = styled.p`
-  font-size: 34px;
-  line-height: 54px;
+  font-size: 24px;
+  line-height: 48px;
   font-weight: 500;
   width: 298px;
   letter-spacing: -0.04em;
@@ -54,6 +74,24 @@ const DescriptText = styled.p`
     border-radius: 60px;
     padding: 0 6px;
   }
+
+  @media screen and (max-width: 820px) {
+    display: none;
+  }
+`;
+
+const DescriptLink = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: #f6f6f6;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 20px;
+  padding-right: 20px;
+  font-size: 30px;
+  font-weight: 900;
+  color: #1f4788;
 
   @media screen and (max-width: 820px) {
     display: none;
