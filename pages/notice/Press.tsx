513597d30b650ed09acd71e8dd11a545664151e8
hgpark <digitalworks_hgpark@amorepacific.com>
2024-05-13 17:05:14 +0900

    [Refactor] 불필요한 as구문 수정 - front pages

--- pages/notice/Press.tsx
+++ pages/notice/Press.tsx
@@ -134,11 +134,11 @@ export const getServerSideProps = async () => {
   const data = await getDocs(queryList);
   const dataList: IPressData[] = [];
   data.forEach((docs) => {
-    const postData = {
+    const postData:IPressData = {
       ...docs.data(),
       id: docs.id,
       date: dayjs(docs.data().createdAt).format('YY-MM-DD'),
-    } as IPressData;
+    };
     dataList.push(postData);
   });
   return { props: { dataList } };
